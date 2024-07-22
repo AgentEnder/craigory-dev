cb22c74ac0f434a17dbdd5e327870a4810e64fc6
Craigory Coppola <craigorycoppola@gmail.com>
2024-07-22 13:20:38 -0400

    chore: remove old deployments

--- 1fe5f5de1766827a49fb629eb72df1e0ff33116d/client/assets/chunks/chunk-Z64xCmXM.js
+++
@@ -1,3 +0,0 @@
-const slides_scss_inline_lang = ".remark-slide-content{background:#6b7594;background:linear-gradient(315deg,#6b7594,#232f4d);color:#f5f5f5}.remark-slide-content a{color:beige}";
-
-export { slides_scss_inline_lang as default };
