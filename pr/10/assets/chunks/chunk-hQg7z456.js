e53ed9975293123382f8a8a7da6fae228bf18f36
AgentEnder <AgentEnder@users.noreply.github.com>
2024-07-23 05:09:46 +0000

    chore: :rocket: deploy new version to Github Pages

---
+++ pr/10/assets/chunks/chunk-hQg7z456.js
@@ -0,0 +1,25 @@
+import { j as jsxRuntimeExports, t as toast } from './chunk-DobrybWg.js';
+
+function ContentMarker() {
+  return /* @__PURE__ */ jsxRuntimeExports.jsx(
+    "div",
+    {
+      style: {
+        display: "inline-block"
+      },
+      onClick: (e) => {
+        if (e.target instanceof HTMLDivElement) {
+          if (e.target.parentElement instanceof HTMLAnchorElement) {
+            if (navigator.clipboard) {
+              navigator.clipboard.writeText(e.target.parentElement.href);
+              toast({ content: "Copied link to clipboard", style: "info" });
+            }
+          }
+        }
+      },
+      children: "#"
+    }
+  );
+}
+
+export { ContentMarker as C };
