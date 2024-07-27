4fda3cf2cdc69762fcad7508a2002fd44aeef8da
Craigory Coppola <craigorycoppola@gmail.com>
2024-07-27 13:57:27 -0400

    feat(craigory-dev): add blog about git aliases (#12)

--- libs/blog-posts/src/index.ts
+++ libs/blog-posts/src/index.ts
@@ -1,2 +1,3 @@
 export { blogPosts, pages, slugMap } from './lib/posts';
 export * from './lib/components/code-wrapper';
+export * from './lib/blog-post';
---
+++ libs/blog-posts/src/lib/posts/superpowered-git-aliases/post.ts
@@ -0,0 +1,10 @@
+import { BlogPost } from '../../blog-post';
+import mdx from './contents.mdx';
+
+export const superpoweredGitAliases: BlogPost = {
+  mdx,
+  publishDate: new Date(2024, 6, 30),
+  slug: 'superpowered-git-aliases',
+  title: 'Superpowered Git Aliases with Scripting',
+  description: `Git aliases are a powerful tool for improving your workflow. In this post, I'll show you how to take them to the next level by using scripting to create aliases that contain control flow and more.`,
+};
